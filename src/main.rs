97d6ac718818ac0117b9e7a7c4583ad00a89ac8c
Ryan <virtualsmile@outlook.com>
2024-02-13 05:39:20 +0800

    Added client listener.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::Ipv4Addr;
+use std::str::FromStr;
+use tokio::io;
+use tokio::net::{TcpListener, TcpStream};
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let server_address = "175.45.183.112:1709";
+    let listener = TcpListener::bind("127.0.0.1:2606").await?;
+
+    loop {
+        let client = listener.accept().await?;
+        let server = TcpStream::connect(server_address).await?;
+    }
 }
