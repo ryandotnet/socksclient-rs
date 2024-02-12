544ebe592412ec49fd1319e8be80b7f27731af84
Ryan <virtualsmile@outlook.com>
2024-02-13 05:42:23 +0800

    Fixed naming convention.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
-use std::net::Ipv4Addr;
-use std::str::FromStr;
 use tokio::io;
 use tokio::net::{TcpListener, TcpStream};
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let server_address = "175.45.183.112:1709";
+    let server_addr = "175.45.183.112:1709";
     let listener = TcpListener::bind("127.0.0.1:2606").await?;
 
     loop {
         let client = listener.accept().await?;
-        let server = TcpStream::connect(server_address).await?;
+        let server = TcpStream::connect(server_addr).await?;
     }
 }
