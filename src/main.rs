b63bc85213e476bb91cb24b7515e3104dccc0b5f
Ryan <virtualsmile@outlook.com>
2024-02-17 02:28:22 +0800

    Updated tests as TCP Proxy.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
+use tokio::io;
 use tokio::net::{TcpListener, TcpStream};
-use tokio::{io, join};
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:2606").await?;
+    println!("Listening on: {}\n", listener.local_addr()?);
 
     loop {
-        let (mut client, _) = listener.accept().await?;
+        let (client, _) = listener.accept().await?;
         handle_client(client).await?;
     }
 }
@@ -15,15 +16,18 @@ async fn handle_client(client: TcpStream) -> io::Result<()> {
     let server_addr = "175.45.183.112:1709";
 
     let server = TcpStream::connect(server_addr).await?;
+    server.set_nodelay(true)?;
 
-    exchange_data(client, server);
+    println!("New connection opened.");
+
+    exchange_data(client, server).await?;
 
     Ok(())
 }
 
-async fn auth_client() {}
+// async fn auth_client() {}
 
-async fn exchange_data(mut client: TcpStream, mut server: TcpStream) {
-    io::copy_bidirectional(&mut client, &mut server);
-    _ = tokio::spawn(async move { io::copy_bidirectional(&mut client, &mut server) })
+async fn exchange_data(mut client: TcpStream, mut server: TcpStream) -> io::Result<()> {
+    tokio::spawn(async move { io::copy_bidirectional(&mut client, &mut server).await });
+    Ok(())
 }
